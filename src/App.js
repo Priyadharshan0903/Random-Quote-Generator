4e72bf4a22114cb038d3fc4239f5489c61c0edea
Priyadharshan <91417662+Priyadharshan0903@users.noreply.github.com>
2023-03-13 00:55:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import QuoteGenerator from "./components/QuoteGenerator";
+
+function App() {
+  return (
+    <div className="App bg-gradient-to-tr from-fuchsia-600 via-transparent via-amber-700  via-slate-400 to-fuchsia-800">
+      <QuoteGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuoteGenerator.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+function QuoteGenerator() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const date = new Date().toLocaleDateString();
+  //   const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  //   const today = new Date().toLocaleDateString(undefined, options);
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      });
+  }, []);
+
+  function handleNewQuote() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      });
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="max-w-lg p-8 bg-slate-900/80  rounded-lg shadow-xl shadow-black">
+        <p className="text-white font-bold text-2xl pb-6  text-end">{date}</p>
+        <p className="text-4xl font-bold text-white text-start ">{quote}</p>
+        <p className="mt-4 text-lg font-medium text-white text-end">
+          <span className=" font-bold text-2xl"> - </span> {author}
+        </p>
+        <button
+          className="mt-8 px-4 py-2 text-2xl font-bold
+           bg-gray-800 text-white rounded
+            hover:bg-gray-700 focus:outline-none
+             focus:ring-2 focus:ring-gray-400 w-[400px] ml-4"
+          onClick={handleNewQuote}
+        >
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default QuoteGenerator;
