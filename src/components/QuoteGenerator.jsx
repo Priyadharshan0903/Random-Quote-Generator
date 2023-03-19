ee44782ba21bd0027fd6a663461c560cef33947d
Priyadharshan <91417662+Priyadharshan0903@users.noreply.github.com>
2023-03-19 19:57:07 +0530

    Update QuoteGenerator.jsx

--- src/components/QuoteGenerator.jsx
+++ src/components/QuoteGenerator.jsx
@@ -26,17 +26,17 @@ function QuoteGenerator() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="max-w-lg p-8 bg-slate-900/80  rounded-lg shadow-xl shadow-black">
+      <div className="max-w-lg p-8 bg-slate-900/80  rounded-lg sm:w-[300px] md:w-[500px] shadow-xl shadow-black">
         <p className="text-white font-bold text-2xl pb-6  text-end">{date}</p>
         <p className="text-4xl font-bold text-white text-start ">{quote}</p>
         <p className="mt-4 text-lg font-medium text-white text-end">
           <span className=" font-bold text-2xl"> - </span> {author}
         </p>
         <button
-          className="mt-8 px-4 py-2 text-2xl font-bold
+          className="mt-8 px-4 py-2 sm:text-md font-bold
            bg-gray-800 text-white rounded
             hover:bg-gray-700 focus:outline-none
-             focus:ring-2 focus:ring-gray-400 w-[400px] ml-4"
+             focus:ring-2 focus:ring-gray-400 md:w-[400px] sm:w-[100px] sm:text-sm md:text-2xl ml-4"
           onClick={handleNewQuote}
         >
           New Quote
